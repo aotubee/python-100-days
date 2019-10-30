ea1ea07bc4dd52feab3b52fc835b7e5378b396a9
kunkaka <406436685@qq.com>
2019-10-29 22:25:38 -0400

    suffix

---
+++ suffix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def get_suffix(filename,has_dot=True):
+    '''
+    获取文件名的后缀
+    :param filename:文件名
+    :param has_dot:返回的后缀名是否需要带点
+    :return:文件的后缀名
+    '''
+
+    pos=filename.rfind('.')
+    if 0<pos<len(filename)-1:
+        index=pos if has_dot else pos+1
+        return filename[index:]
+    else:
+        return ''
+
+if __name__=='__main__':
+    print (get_suffix('test.doc'))
