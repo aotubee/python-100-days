b464ad771b414c20b29360a337b442a8b3e968ac
uya <406436685@qq.com>
2022-11-16 18:09:26 +0800

    增加了@组成员功能

--- tools/dingding_demo.py
+++ tools/dingding_demo.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3
-# coding=utf-8
+#coding=utf-8
 
 import time
 import hmac
 import hashlib
 import base64
+import requests
+import json
 import urllib.parse
-import requests,json
 
 
 #钉钉机器人的timestap和加签，加签在设置钉钉机器人时生成。下面代码引用钉钉加签说明文档
 timestamp = str(round(time.time() * 1000))
-secret = 'SEC0a48585c3fc279b43713e6ae7a8fc6dfc0624f7fc28f46e96b2488d0f305ad95'
+secret = 'SEC8977ad824079c46935488be99fb3bc87f92a61886c21ad03bdc312cf3ed1c0a0'
 secret_enc = secret.encode('utf-8')
 string_to_sign = '{}\n{}'.format(timestamp, secret)
 string_to_sign_enc = string_to_sign.encode('utf-8')
 hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
 sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-print(timestamp)
-print(sign)
+#print (timestamp)
+#print (sign)
 
 #https://oapi.dingtalk.com/robot/send?access_token=XXXXXX&timestamp=XXX&sign=XXX
-#
-token='113bcd615ce5cceaa48f42e4ed59bf36f481d288a795d61780017f550e6d816b'
-#
+
+token='867b3499753420ea8d9f4d67e560c27e6965594e58ecfd9c5283f4b9156643db'
 webhook ='https://oapi.dingtalk.com/robot/send?access_token=' + token + '&timestamp=' + timestamp + '&sign=' + sign
-print (webhook)
+#print (webhook)
 
 data = {
-    "msgtype": "text", 
+    "msgtype": "text",
     "text": { "content": "测试" },
+    #@群成员
+    "at": {"atMobiles": ["18602902055"]},
     "isAtAll": False }
 
 headers={'Content-Type': 'application/json'}   #定义数据类型
@@ -37,3 +39,4 @@ req = requests.post(webhook, data=json.dumps(data), headers=headers)   #发送po
 #req = requests.get(url=webhook)
 html = req.text
 print(html)
+print(json.dumps(data))
